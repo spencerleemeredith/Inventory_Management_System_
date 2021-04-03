416a549a49a65538f066901a001634542ecb8064
Spencer Lee Meredith <spencerleemeredith@gmail.com>
2021-04-03 14:43:25 -0700

    ✏️ fix typos 🌱add table for products ✨add vue router ✨add vue ui from laravel 💄add  stylesheets

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
   <meta name="author" content="">
   <link href="{{ asset ('backend/img/logo/logo.png')}}" rel="icon">
   <title>RuangAdmin - Dashboard</title>
-  <link rel="stylesheet" type="text/css" href="{{ assets('css/app.css')}}">
+  <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css')}}">
   <link href="{{ asset ('backend/vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
   <link href="{{ asset ('backend/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet" type="text/css">
   <link href="{{ asset ('backend/css/ruang-admin.min.css')}}" rel="stylesheet">
