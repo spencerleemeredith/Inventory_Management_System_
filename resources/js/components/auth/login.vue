141d2fc142f9364de03821d64175a02e9ae3665d
Spencer Lee Meredith <spencerleemeredith@gmail.com>
2021-04-03 18:08:26 -0700

    ✨  add routes component 🚧 working on login and register components 💥add auth folder structure

---
+++ resources/js/components/auth/login.vue
@@ -0,0 +1,7 @@
+<template>
+    <h1>Login Form</h1>
+</template>
+
+<script type="text/javascript"></script>
+
+<style type="text/css"></style>
---
+++ resources/js/components/auth/register.vue
@@ -0,0 +1,7 @@
+<template>
+    <h1>Registration Form</h1>
+</template>
+
+<script type="text/javascript"></script>
+
+<style type="text/css"></style>
