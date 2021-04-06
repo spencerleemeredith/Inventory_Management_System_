609b33634e63feff130ce80bd1636c427ef7a857
Spencer Lee Meredith <spencerleemeredith@gmail.com>
2021-04-05 17:39:31 -0700

    ✨add employee controller ✨add create component 🚧working on create component ✨add Employee model 🔧configure employee model

---
+++ app/Models/Model/Employee.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models\Model;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Employee extends Model
+{
+     protected $fillable = [
+        'name', 'email', 'phone','address','salary','photo','nid','joining_date'
+    ];
+}
